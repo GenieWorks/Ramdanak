780776f5cbb0f3ce5afe5076fe315669b885b73f
ibrahimhussiny <ibrahim_hussiny@hotmail.com>
2015-03-27 23:56:06 +0200

    Revert "listView that will be used to show series info ,missing showing the rank as stars (i will serach for it isa)."
    This reverts commit 9cc6009e87d8f757c9365ea7a246076b8121b81e.

--- src/main/java/com/Ramdanak/ramdank/Main.java
+++ src/main/java/com/Ramdanak/ramdank/Main.java
@@ -1,7 +1,6 @@
 package com.Ramdanak.ramdank;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,8 +14,6 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(Main.this,allSeries.class));
-        finish();
     }
 
 
--- src/main/java/com/Ramdanak/ramdank/allSeries.java
+++ src/main/java/com/Ramdanak/ramdank/allSeries.java
@@ -1,77 +1,22 @@
 package com.Ramdanak.ramdank;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.graphics.Bitmap;
-import android.graphics.Movie;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
-
 import com.Ramdanak.ramdank.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /*
     shows list of all programmes and series.
  */
 public class allSeries extends Activity {
 
-    private ProgressDialog pDialog;
-    private ArrayList<seriesInfo> seriesList ;
-    private ListView listView;
-    private MyCustomBaseAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_series);
-
-        init();
     }
 
-    private void init(){
-        seriesList= new ArrayList<seriesInfo>();
-        listView = (ListView) findViewById(R.id.srListView);
-        adapter = new MyCustomBaseAdapter(this, seriesList);
-        listView.setAdapter(adapter);
-
-        pDialog = new ProgressDialog(this);
-        // Showing progress dialog before making http request
-        pDialog.setMessage("Loading...");
-        pDialog.show();
-        //add items to the list view
-        //first one
-        seriesInfo tempSeries=new seriesInfo();
-        tempSeries.setRating(4.5);
-        tempSeries.setTitle("الكبير");
-        Drawable myDrawable = getResources().getDrawable(R.drawable.kabeer);
-        Bitmap myLogo = ((BitmapDrawable) myDrawable).getBitmap();
-        tempSeries.setImg(myLogo);
-        seriesList.add(tempSeries);
-        adapter.notifyDataSetChanged();
-        //second
-        seriesInfo tempSeries2=new seriesInfo();
-        tempSeries2.setRating(3.2);
-        tempSeries2.setTitle("الكبير اوى");
-        tempSeries2.setImg(myLogo);
-        seriesList.add(tempSeries2);
-        adapter.notifyDataSetChanged();
-
-        hidePDialog();
-    }
-
-
-    private void hidePDialog() {
-        if (pDialog != null) {
-            pDialog.dismiss();
-            pDialog = null;
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
