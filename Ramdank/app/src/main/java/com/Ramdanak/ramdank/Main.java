d2a1455151565c4fb953fec8906d3ba2aebb2085
ibrahimhussiny <ibrahim_hussiny@hotmail.com>
2015-04-04 12:23:41 +0200

    navigation between (allSeries ,allChannels ,favorite series ,favorite channels) using fragment tabHost

--- Ramdank/app/src/main/java/com/Ramdanak/ramdank/Main.java
+++ Ramdank/app/src/main/java/com/Ramdanak/ramdank/Main.java
@@ -1,29 +1,56 @@
 package com.Ramdanak.ramdank;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+
+
 /*
-  Main Activity of the app show options to the user (show all Series ,Channels or show your favorites)
+    Tabbed Activity to show(allSeries ,allChannels ,favourite series,favourite channels).
  */
-public class Main extends Activity {
+public class Main extends FragmentActivity {
+
+
+    private FragmentTabHost mTabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(Main.this,AndroidTabLayoutActivity.class));
-        finish();
+        setContentView(R.layout.activity_all_series);
+
+        init();
     }
+
+    private void init(){
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("allSeriesTab").setIndicator("كل المسلسلات", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("allChannelsTab").setIndicator("كل القنوات", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("favouriteSeriesTab").setIndicator("المسلسلات المفضله", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("favouriteChannelsTab").setIndicator("القنوات المفضله", null),
+                FragmentTab.class, null);
+    }
+
+
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.all_series, menu);
         return true;
     }
 
