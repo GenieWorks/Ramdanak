5562feeb8d44e499b50162568f4508a19583190c
mohamed-moanis <mohamed_moanis@outlook.com>
2015-04-20 17:26:37 +0200

    .gitignores

---
+++ database.sql
@@ -0,0 +1,30 @@
+CREATE TABLE SHOW (
+    _id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    trailer TEXT NOT NULL,
+    logo BLOB NOT NULL,  -- add default logo
+    -- rating_count INTEGER NOT NULL,
+    rating REAL NOT NULL
+)
+
+
+CREATE TABLE CHANNEL (
+    _id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    frequency TEXT NOT NULL,
+    code TEXT NOT NULL,
+    vertical INTEGER NOT NULL,
+    logo BLOB NOT NULL,
+    -- rating_count INTEGER NOT NULL,
+    rating REAL NOT NULL
+)
+
+CREATE TABLE TV_RECORD (
+    _id INTEGER PRIMARY KEY,
+    channel_id INTEGER,
+    show_id INTEGER,
+    start_time TEXT NOT NULL,
+    end_time TEXT NOT NULL,
+    FOREIGN KEY(channel_id) REFERENCES CHANNEL(id),
+    FOREIGN KEY(show_id) REFERENCES SHOW(id)
+)
