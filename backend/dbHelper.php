290037333d8a92da599e08a037828433ee95909d
mohamed <mohamed_moanis@outlook.com>
2015-05-10 19:11:25 +0300

    renamed config.php to dbHelper.php It will manage all functions for database
    Also finished the functions for updating ratings

---
+++ backend/dbHelper.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Open a connection to the database
+ * @return mixed, false on failure or the connection on success
+ */
+function open_database() {
+    $connection = mysqli_connect('localhost','root');
+
+    // Check connection
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    // TODO: add database name
+    if (!mysqli_select_db($connection, "")) {
+        return false;
+    }
+
+    return $connection;
+}
+
+/**
+ * update the rating for a certain tvshow
+ * @param int $show show id
+ * @param decimal $user_rating user rating
+ * @return boolean, TRUE on sccuess and FALSE on failure
+ */
+function update_show_rating($show, $user_rating) {
+    // get the rating count and the current rating for the show
+    $query = "SELECT Rating_count, Rating FROM Show WHER Show_ID = " . $show;
+    $result = mysql_query($query);
+    
+    if (!$result) {
+        die('Could not query:' . mysql_error());
+    }
+    
+    $rating_count = mysql_result($result, 0);
+    $rating = mysql_result($result, 1);
+    
+    // calculate the new rating
+    $new_rating = ($rating_count * $rating + $user_rating) / ($rating_count + 1);
+    
+    $query = "UPDATE Show SET Rating_count = " . ($rating_count + 1)
+            . " Rating = " . $new_rating . " WHERE Show_ID = " . $show;
+    
+    return mysql_query($query);
+}
+
+/**
+ * Updates the rating for a channel
+ * @param int $channel channel id
+ * @param decimal $user_rating user rating
+ * @return boolean, TRUE on success and FALSE on failure
+ */
+function update_channel_rating($channel, $user_rating) {
+    // get the rating count and the current rating for the channel
+    $query = "SELECT Rating_count, Rating FROM Channel WHER Channel_ID = " . $channel;
+    $result = mysql_query($query);
+    
+    if (!$result) {
+        die('Could not query:' . mysql_error());
+    }
+    
+    $rating_count = mysql_result($result, 0);
+    $rating = mysql_result($result, 1);
+    
+    // calculate the new rating
+    $new_rating = ($rating_count * $rating + $user_rating) / ($rating_count + 1);
+    
+    $query = "UPDATE Channel SET Rating_count = " . ($rating_count + 1)
+            . " Rating = " . $new_rating . " WHERE Channel_ID = " . $channel;
+    
+    return mysql_query($query);
+}
+
+/**
+ * Closes a given mysql connection
+ * @param mysql_connection $connection current open connection
+ */
+function close_connection($connection) {
+    if ($connection != NULL || $connection != FALSE) {
+        mysql_close($connection);
+    }
+}
