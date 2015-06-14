0b779b5bf793dd00e8b06974642b74aafc9a7eea
mohamed <mohamed_moanis@outlook.com>
2015-06-15 01:19:27 +0200

    suppress warning

--- Ramdank/app/src/main/java/com/Ramdanak/ramdank/NetworkManager.java
+++ Ramdank/app/src/main/java/com/Ramdanak/ramdank/NetworkManager.java
@@ -7,7 +7,7 @@ import android.net.ConnectivityManager;
  * Checks for network connection
  * Created by mohamed on 4/21/15.
  */
-public class NetworkManager {
+public abstract class NetworkManager {
 
     /**
      * Initializes the client class
@@ -15,7 +15,7 @@ public class NetworkManager {
      * @param context application context of the main activity.
      */
     public static boolean checkInternetOpened(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected()
         ||
         connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isRoaming();
